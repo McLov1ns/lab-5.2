8191f33f2df9a5327f61445a3b3cb7a21f37e165
суперкомп <@DEMIN-ALEX>
2023-03-18 17:44:52 +0700

    Добавьте файлы проекта.

---
+++ lab 5.2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lab_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Check();
+        }
+        public class Book
+        {
+            public string Title { get; set; }
+        }
+
+        public static void Check()
+        {
+            var book = new Book();
+            book.Title = "Structure and interpretation of computer programs";
+            Console.WriteLine(book.Title);
+        }
+    }
+}
